cfef33a7cbf30f91e6eca4c52331265e696c1f6d
Artem Shevchenko <bigforko@gmail.com>
2023-11-12 16:28:32 +0100

    Create webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+const { VueLoaderPlugin } = require('vue-loader');
+const path = require('path');
+
+module.exports = {
+  mode: 'production',
+  entry: './vue-timepicker.vue',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'vue-timepicker.js',
+    library: 'VueTimepicker',
+    libraryTarget: 'umd',
+    umdNamedDefine: true
+  },
+  externals: {
+    vue: 'Vue'
+  },
+  resolve: {
+    extensions: ['.ts', '.js', '.vue'],
+    alias: {
+      'vue$': 'vue/dist/vue.esm-bundler.js'
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader'
+      },
+      {
+        test: /\.ts$/,
+        loader: 'ts-loader',
+        options: {
+          appendTsSuffixTo: [/\.vue$/],
+        },
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  },
+  plugins: [
+    new VueLoaderPlugin()
+  ]
+};
