4c0c3f45795e60be7fbbee58ab9d698139ce8315
Artem Shevchenko <bigforko@gmail.com>
2023-11-13 20:47:23 +0100

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 module.exports = {
   mode: 'production',
-  entry: './vue-timepicker.vue',
+  entry: './src/vue-timepicker.vue',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'vue-timepicker.js',
